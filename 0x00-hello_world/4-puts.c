b085f6cd730d8cca5f998026f698c7cc7e665923
edgar <edgar-william@hotmail.com>
2021-10-04 21:05:37 +0000

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
  * main - entry pint
  * return: 0 number
  */
-int main (void)
-	
+int main(void)
 {
-         puts ("\"Programming is like building a multilingual puzzle");
-
-	 return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
