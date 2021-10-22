5e9f43706e3bcf5f3ac5997a4dbe8660505b91a5
edgar <edgar-william@hotmail.com>
2021-10-22 02:55:27 +0000

    The prototypes of all your functions and the prototype of the function _putchar

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,12 @@
 #ifndef HEADER
 #define HEADER
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int fun_srqt(int n, int a);
+int is_prime_number(int n);
 #endif
