3738bb2015097788ee587ddbb88eee3db7644edf
edgar <edgar-william@hotmail.com>
2021-10-28 04:55:46 +0000

    function that returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strdup - function that concatenates two strings
+ * @str: char
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int acum = 0;
+	int x = 0;
+	char *ma;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[x] != '\0')
+	{
+		acum += 1;
+		x++;
+	}
+	ma = malloc(sizeof(char) * (acum + 1));
+	if (!ma)
+	{
+		return (NULL);
+	}
+	x = 0;
+	while (str[x] != '\0')
+	{
+		ma[x] = str[x];
+		x++;
+	}
+	ma[x] = '\0';
+	return (ma);
+}
