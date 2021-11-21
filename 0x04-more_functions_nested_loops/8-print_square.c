9e0fe72807aeb0f68ebb87e97d1b5167753b3b0f
edgar <edgar-william@hotmail.com>
2021-11-21 05:25:56 +0000

    function that prints a square, followed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * @size: int
+ */
+void print_square(int size)
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < size)
+	{
+		while (y < size)
+		{
+			_putchar(35);
+			y++;
+		}
+		_putchar(10);
+		x++;
+	}
+	if (x <= 0)
+	{
+		_putchar(10);
+	}
+}
