118da81701ab6342146d4db639c1ba82dce20548
edgar <edgar-william@hotmail.com>
2021-12-14 05:12:44 +0000

    program that computes and prints the sum of all the multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that computes and prints the sum of all
+ * the multiples
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			b = b + x;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
